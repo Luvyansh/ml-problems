d7d65896d666fd22a7298e159c75a7930d564587
Anurag Pandey <85816783+Luvyansh@users.noreply.github.com>
2026-06-11 17:44:32 +0530

    Update MLP from scratch (Build a Neural Net 4/5)

--- foundations/mlp.py
+++ foundations/mlp.py
@@ -10,9 +10,15 @@ class Solution:
         # biases: list of 1D bias vectors
         # Apply ReLU after each hidden layer, no activation on output layer
         # return np.round(your_answer, 5)
-        h = x
-        for i in range(len(weights)):
-            h = h @ weights[i] + biases[i]
-            if i < len(weights) - 1:
-                h = np.maximum(0, h)
-        return np.round(h, 5)
+        h= x
+        num_layers = len(weights)
+
+        for i in range(num_layers):
+            z = np.dot(h,weights[i]) + biases[i]
+
+            if i < num_layers -1 :
+                h = np.maximum(0,z)
+            else:
+                h = z
+
+        return np.round(h,5)
